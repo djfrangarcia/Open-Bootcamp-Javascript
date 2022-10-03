819d07eab50f096fbf2e041061a6a6afdae6b657
Francisco Jesus Miguel <djfrangarcia@hotmail.com>
2022-10-03 12:26:20 +0200

    OK

---
+++ mi-primer-proyecto-de-node/index.js
@@ -0,0 +1,10 @@
+
+//Este es el mensaje de bienvenida
+console.log("esta es la puerta de entrada al proyecto")
+/*
+Esta es la parte intermedia de nuestro proyecto
+En esta parte realizamos los chuequeos necesarios
+Por ultimo vamos a la ultims linea de saludo
+*/
+//Este es el mensaje de despedida
+console.log("Adios")
