6225cdf20863e882dc38cb798cb4f181fe05388b
Francisco Jesus Miguel <djfrangarcia@hotmail.com>
2022-10-04 10:21:22 +0200

    segundo ejercicio hecho

---
+++ segundo-ejercicio/Segundo ejercicio.js
@@ -0,0 +1,12 @@
+const misDatos = ["Fran", 38, true, new Date(1984, 4, 5)];
+
+const libroFavorito = {
+    titulo: "Cementerio de animales",
+    autor:"Stephen King",
+    fecha: 1983,
+    url: "https://cinedemedianoche.files.wordpress.com/2017/05/cinedemedianoche.pdf"
+}
+
+console.log(misDatos);
+
+console.log(libroFavorito);
